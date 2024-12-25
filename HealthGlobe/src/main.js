0bdfcefb5e969beff5fb8f5ae07daa21ebb6b036
brandonnwongg <130675356+brandonnwongg@users.noreply.github.com>
2024-12-25 04:32:46 +0100

    Basic Sphere Starting Point + Resizing Option

--- HealthGlobe/src/main.js
+++ HealthGlobe/src/main.js
@@ -1,28 +1,53 @@
-import * as THREE from 'three'
+import * as THREE from "three";
+import getLayer from "./getLayer.js";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
+const w = window.innerWidth;
+const h = window.innerHeight;
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(1, 32, 32);
-const material =new THREE.MeshBasicMaterial({ color: 0x00ff00})
+const ctrls = new OrbitControls(camera, renderer.domElement);
+ctrls.enableDamping = true;
 
-const mesh = new THREE.Mesh(geometry, material)
+// Basic Sphere Geometry for an Earth Globe Model
+const geometry = new THREE.SphereGeometry();
+const material = new THREE.MeshStandardMaterial({
+  color: 0x454B1B, //Hex Colour of Army Green
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-scene.add(mesh)
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemiLight);
 
-const temp = {
-  width: 1024,
-  height: 720
+// Sprites BG
+const gradientBackground = getLayer({
+  hue: 0.9,
+  numSprites: 8,
+  opacity: 0.2,
+  radius: 10,
+  size: 30,
+  z: -15.5,
+});
+//scene.add(gradientBackground);
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.y += 0.05;
+  renderer.render(scene, camera);
+  ctrls.update();
 }
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  temp.width / temp.height
-)
+animate();
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(temp.width, temp.height)
-document.body.appendChild(renderer.domElement)
-
-camera.position.z = 4
-
-renderer.render(scene, camera)
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
