c16bdc6017ab689ad068b0f01765d62f60034a47
brandonnwongg <130675356+brandonnwongg@users.noreply.github.com>
2025-01-14 10:34:56 +0100

    Final Earth Look

--- HealthGlobe/vite-project/main.js
+++ HealthGlobe/vite-project/main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import gsap from "./node_modules/gsap/index.js";
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -82,7 +84,6 @@ const sphere = new THREE.Mesh(
   })
 )
 
-scene.add(sphere)
 
 // Creating a atmosphere
 const atmosphere = new THREE.Mesh(
@@ -100,12 +101,57 @@ atmosphere.scale.set(1.1, 1.1, 1.1)
 
 scene.add(atmosphere)
 
+const group = new THREE.Group()
+group.add(sphere)
+scene.add(group)
+
+
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial(
+  {
+    color: 0xffffff
+  })
+
+// spawn random stars
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+  const x = (Math.random() - 0.5) * 2000
+  const y = (Math.random() - 0.5) * 2000
+  const z = -Math.random() * 3000
+  starVertices.push(x,y,z)
+}
+
+starGeometry.setAttribute('position', 
+new THREE.Float32BufferAttribute(
+  starVertices, 3
+))
+
+const stars = new THREE.Points(
+  starGeometry, starMaterial)
+scene.add(stars)
+
 camera.position.z = 15
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
 
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
   sphere.rotation.y += 0.001
+  gsap.to(group.rotation, {
+    x: -mouse.y * 0.5,
+    y: mouse.x * 0.5,
+    duration: 2
+  })
 }
 animate()
+
+
+addEventListener('mousemove', () => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1
+})
 
